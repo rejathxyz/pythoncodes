8519131414afff070fed2c05a7c5b887ab3bf79c
rj777pain <rejathtm@gmail.com>
2022-10-02 20:14:10 +0530

    solved sum of digits

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,1 +1,14 @@
 #to find the sums of digits of number till it becomes one digit value
+
+def sum_of_digits(n):
+    if (n == 0):
+        return 0
+    mod_value = n % 9
+    if(mod_value == 0):
+        return 9
+    else:
+        return mod_value
+
+n = int(input("Enter the number: "))
+print(sum_of_digits(n))
+    
