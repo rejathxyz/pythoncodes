ce2015f7988204adf17a57e016b4d31fea66d65e
Rj777pain <rejathtm@gmail.com>
2023-10-08 20:54:55 +0530

    added description and updated output message for sum of digits

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,5 +1,7 @@
-#to find the sums of digits of number till it becomes one digit value
-
+# to find the sums of digits of number till it becomes one digit value
+# this program will take the mod the input number if return the value as sum of its digits to 1 digit. 
+# if the number is 0  or a multiple of 9 the result will be 0. If the number is 0 we will return it as 0 otherwise
+# we will return it as 9 in case the mod is 0
 def sum_of_digits(n):
     if (n == 0):
         return 0
@@ -10,5 +12,5 @@ def sum_of_digits(n):
         return mod_value
 
 n = int(input("Enter the number: "))
-print(sum_of_digits(n))
+print("Sum of the digit is : ",sum_of_digits(n))
     
