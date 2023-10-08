1b542c3056b410a5eb75774fb90fbf61918bd304
Abhishek Raj Nair <abhishek.raj@batonsystems.com>
2023-10-08 20:34:17 +0530

    added logic for generating password

---
+++ passord_generator.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+
+def generate_password(length=12):
+  letters = string.ascii_letters
+  digits = string.digits
+  special_chars = string.punctuation
+
+  all_chars = letters + digits + special_chars
+
+  password = [
+      random.choice(letters),
+      random.choice(letters.upper()),
+      random.choice(digits),
+      random.choice(special_chars),
+  ]
+
+  password += random.choices(all_chars, k=length - 4)
+
+  random.shuffle(password)
+
+  return ''.join(password)
+
+
+password = generate_password(16)
+print("Random Password:", password)
